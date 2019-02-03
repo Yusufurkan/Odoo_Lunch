08b4c9fa734144f208dcaba35c4f1bee163a933d
Big Z <ziskan1013@gmail.com>
2019-02-03 17:34:31 -0500

    updated test description

--- src/test/java/com/odoo/tests/managerTests/employeePaymentsTests/EmployeePaymentsTests.java
+++ src/test/java/com/odoo/tests/managerTests/employeePaymentsTests/EmployeePaymentsTests.java
@@ -1,15 +1,12 @@
 package com.odoo.tests.managerTests.employeePaymentsTests;
 
 import com.odoo.utilities.TestBase;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class EmployeePaymentsTests extends TestBase {
 
-    @Test
+    @Test (description = "Verify that manager can see transactions by payment")
     public void EmployeeTransactionsDIsplayedTest() throws Exception {
         extentLogger = report.createTest("Employee Payments Test.");
         extentLogger.info("Sign into 'Lunch' module");
@@ -19,6 +16,5 @@ public class EmployeePaymentsTests extends TestBase {
        extentLogger.info("Verify that employee payment transactions are displayed");
        Assert.assertTrue(pages.employeePaymentsPage().transactionAmount.size() > 0, "No payment transactions found");
        extentLogger.pass( pages.employeePaymentsPage().transactionAmount.size() + " employee transactions are displayed.");
-
     }
 }
